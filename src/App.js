2c4c77f65c90f75b1b4ee05363cc5c33c3c22a30
rutkat <tom@runastartup.com>
2019-10-04 19:23:32 -0500

    Rename to default App name.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './app.css';
+import './App.css';
 import Header from './components/Header';
 import HotelDetails from './components/HotelDetails';
 import Footer from './components/Footer';
